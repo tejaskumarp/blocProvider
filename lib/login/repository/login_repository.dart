50b7b73e18f172264974ec8e7effb680a85f34c7
tejaskumarp <Tejaskumar.p@coforge.com>
2024-10-02 08:53:24 +0530

    Login message change

--- lib/login/repository/login_repository.dart
+++ lib/login/repository/login_repository.dart
@@ -1,6 +1,6 @@
 class LoginRepository {
   Future<void> login() async {
-    await Future.delayed(const Duration(seconds: 20));
+    await Future.delayed(const Duration(seconds: 10));
     throw Exception('login failed');
   }
 }
